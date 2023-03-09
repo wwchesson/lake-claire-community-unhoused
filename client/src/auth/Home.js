15d9d68f50339192470961b174de227224a86c41
Will <willchesson@gmail.com>
2023-03-09 15:10:11 -0500

    home page

--- client/src/auth/Home.js
+++ client/src/auth/Home.js
@@ -1,60 +1,62 @@
 import React, { useState } from "react";
 import LoginForm from "./LoginForm";
 import SignupForm from "./SignupForm";
-import {LoginCard} from "../components/StyleElements"
-import { Card, CardContent, CardMedia, Typography } from "@mui/material";
-import garden from "./garden.jpeg"
+import { LoginCard } from "../components/StyleElements";
+import { Card, CardContent, Button, Typography } from "@mui/material";
+import {positions} from '@mui/system'
+import garden from "./garden.jpeg";
 
-function Home ({onLogin}) {
+function Home({ onLogin }) {
+  const [showLogin, setShowLogin] = useState(false);
 
-    const [showLogin, setShowLogin] = useState(true);
-
-    return (
-        <div>
-        {/* <Card sx={{ bgcolor: "blue"}}>
-            <CardContent
-            sx={{
-                margin: "auto",
-            display: "flex",
-            justifyContent: "center",
-            maxWidth: "800px"
-            }}
-            >
-
-            </CardContent>
-        </Card> */}
-        <Card sx={{ display: "flex"}}>
-            <CardContent style={{ backgroundImage: `url(${garden})`, backgroundRepeat: "no-repeat", backgroundSize: "cover", height: "275px", width: "100vw", backgroundPosition: "center", justifyContent: "center", margin: "auto", display: "flex", }}>
-
-                <Typography variant="h3" marginTop="200px" color="white"><strong>The Lake Claire Community for the Unhoused</strong></Typography>
-            </CardContent>
-        </Card>
-        <h2 className="home-text">Manager Login Page</h2>
-        <LoginCard>
-            {showLogin ? (
-            <>
+  return (
+    <div>
+        {showLogin ? (
+          <>
             <LoginForm onLogin={onLogin} />
             <br />
             <p>
-            Create an account &nbsp;
-            <button onClick={() => setShowLogin(false)}>Sign up</button>
+              Create an account &nbsp;
+              <button onClick={() => setShowLogin(false)}>Sign up</button>
             </p>
-            </>
-        ) : (
-            <>
-            <SignupForm onLogin={onLogin} />
-            <br />
-            <p>
-            Already have an account? &nbsp;
-            <button onClick={() => setShowLogin(true)}>Log In</button>
-            </p>
-            </>
-        ) }
-        </LoginCard>
+          </>
+        ) : (null
+        //   <>
+        //     <SignupForm onLogin={onLogin} />
+        //     <br />
+        //     <p>
+        //       Already have an account? &nbsp;
+        //       <button onClick={() => setShowLogin(true)}>Log In</button>
+        //     </p>
+        //   </>
+        )}
 
 
-        </div>
-    )
+      <Card sx={{ display: "flex" }}>
+        <CardContent
+          style={{
+            backgroundImage: `url(${garden})`,
+            backgroundRepeat: "no-repeat",
+            backgroundSize: "cover",
+            height: "40vw",
+            width: "100vw",
+            backgroundPosition: "center",
+            justifyContent: "center",
+            margin: "auto",
+            display: "flex",
+          }}
+        >
+          <Typography display="flex" variant="h3" marginTop="350px" color="white">
+            <strong>The Lake Claire Community for the Unhoused</strong>
+          </Typography>
+          <br />
+          <Button onClick={() => setShowLogin(!showLogin)} sx={{position: "fixed", top: "425px"}}>
+            <Typography variant="h5" color="white"><strong>Login</strong></Typography>
+          </Button>
+        </CardContent>
+      </Card>
+    </div>
+  );
 }
 
-export default Home
+export default Home;
