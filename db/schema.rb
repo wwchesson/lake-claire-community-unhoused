f3c862a5fc5a862b913971e70679d636d07738d7
Will <willchesson@gmail.com>
2023-03-09 12:55:30 -0500

    staff migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_03_08_190853) do
+ActiveRecord::Schema.define(version: 2023_03_09_175240) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,11 @@ ActiveRecord::Schema.define(version: 2023_03_08_190853) do
     t.string "time_of_day"
     t.integer "teacher_id"
     t.integer "resident_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "administrators", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
@@ -51,6 +56,11 @@ ActiveRecord::Schema.define(version: 2023_03_08_190853) do
     t.string "address"
     t.string "emer_name"
     t.string "emer_phone"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "therapists", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
