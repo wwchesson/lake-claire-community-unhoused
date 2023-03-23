bc5f852f9d1491272fa454a5b2b6ba0ae22a790a
Will <willchesson@gmail.com>
2023-03-23 15:39:56 -0400

    activities

--- client/src/components/ResidentFullProfile.js
+++ client/src/components/ResidentFullProfile.js
@@ -1,32 +1,44 @@
-import React, {useEffect, useState} from "react";
-import {useParams} from "react-router-dom"
-import {Box, Card, CardContent, Avatar, Typography, Button } from "@mui/material"
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import {
+  Box,
+  Card,
+  CardContent,
+  Avatar,
+  Typography,
+  Button,
+} from "@mui/material";
 
+function ResidentFullProfile({ onActClick, showActivities }) {
+  const { id } = useParams();
+  const [individualRes, setIndividualRes] = useState([]);
 
-function ResidentFullProfile({onActClick, showActivities}) {
-    const { id } = useParams();
-    const [individualRes, setIndividualRes] = useState([])
+  const {
+    name,
+    image,
+    email,
+    date_of_birth,
+    phone,
+    resident_dorm,
+    res_activities,
+  } = individualRes;
 
-    const {name, image, email, date_of_birth, phone, resident_dorm} = individualRes
+  useEffect(() => {
+    fetch(`${id}`)
+      .then((r) => r.json())
+      .then((data) => {
+        console.log(data);
+        setIndividualRes(data);
+      });
+  }, [id]);
 
-    useEffect(() => {
-        fetch(`${id}`)
-        .then(r => r.json())
-        .then(data => {
-            console.log(data);
-setIndividualRes(data)
+  console.log(showActivities);
 
-        })
-    }, [id])
-
-console.log(showActivities)
-
-    return (
-<div className="full-profile">
-
-
-
- <Card
+  return (
+    <div className="full-profile">
+      {showActivities ? (
+        <div>
+                <Card
           sx={{
             width: "350px",
             height: "400px",
@@ -58,32 +70,74 @@ console.log(showActivities)
             sx={{
               justifyContent: "space-evenly",
               alignItems: "center",
-              marginLeft: "80px"
+              marginLeft: "80px",
               // display: "flex",
             }}
-            >
-            <Typography variant="h6" >
-                <strong>{name}</strong>
+          >
+          
+
+          </CardContent>
+        </Card>
+        </div>
+      ) : (
+        <Card
+          sx={{
+            width: "350px",
+            height: "400px",
+            padding: "40px",
+            borderRadius: "16px",
+            border: 1,
+            margin: "75px",
+            justifyContent: "center",
+            backgroundColor: "#f6f7c3",
+          }}
+        >
+          <Box
+            // className="resident-profile"
+
+            alignItems="center"
+            display="flex"
+            // margin="auto"
+            flexDirection="column"
+            flexGrow="1"
+          >
+            <Avatar
+              alt={name}
+              src={image}
+              sx={{ width: 150, height: 150 }}
+            ></Avatar>
+          </Box>
+
+          <CardContent
+            sx={{
+              justifyContent: "space-evenly",
+              alignItems: "center",
+              marginLeft: "80px",
+              // display: "flex",
+            }}
+          >
+            <Typography variant="h6">
+              <strong>{name}</strong>
             </Typography>
             <br />
             <Typography>
-                <strong>DOB:</strong>{date_of_birth}
+              <strong>DOB:</strong>
+              {date_of_birth}
             </Typography>
             <Typography>
-                <strong>Phone:</strong> {phone}
+              <strong>Phone:</strong> {phone}
             </Typography>
             <Typography>
-                <strong>Email:</strong> {email}
+              <strong>Email:</strong> {email}
             </Typography>
             <Typography>
-                <strong>Dorm:</strong> {resident_dorm}
+              <strong>Dorm:</strong> {resident_dorm}
             </Typography>
-        </CardContent>
+          </CardContent>
         </Card>
-        </div>
-
-
-    )
+      )}
+    </div>
+  );
 }
 
-export default ResidentFullProfile
+export default ResidentFullProfile;
